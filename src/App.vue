2e526a83b6356ac29a1b1b49cdcc09367d8e0ba1
Trevor-143 <trevorarapu2@gmail.com>
2022-08-12 16:53:40 +0300

    failed to delete todo though

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,94 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Hey, <input type="text" placeholder="Type your name" v-model="name"> 
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>Create a toDo</h3>
+      <form @submit.prevent="addTodo">
+        <h4>Whats on for today</h4>
+        <input type="text" placeholder="e.g make a video" v-model="input_content">
+        <h4>Pick a category</h4>
+        <div class="options">
+
+          <label>
+            <input type="radio" name="category" value="business" v-model="input_category">
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input type="radio" name="category" value="personal" v-model="input_category">
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+
+          <input type="submit" value="add toDo">
+        </div>
+      </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>toDo list</h3> 
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label >
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const todos = ref([])
+const name = ref('')
+const input_content = ref('')
+const input_category = ref(null)
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+}))
+
+
+const addTodo = () => {
+  if(input_content.value.trim() === '' || input_content.value === null) {
+    return
   }
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
+  })
 }
-</script>
+const removeTodo = todo => {
+  todos.value = todos.value.filter(t => t !== todo)
+}
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+watch(todos, (newVal) => {
+  localStorage.setItem('todos', JSON.stringify(newVal))
+}, {deep: true})
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos') || [])
+})
+</script>
